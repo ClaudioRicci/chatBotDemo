16c14280251c6b69a2f049942976fe4e107b4ff3
Claudio Ricci <claudioricci@Claudios-MBP.home>
2019-10-21 15:07:48 +0100

    add read-me

---
+++ src/components/tomato/Tomato.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Tomato() {
+  return (
+    <>
+      <div className="tomatoes">
+        <div className="tomato-1" />
+        <div className="tomato-2" />
+        <div className="tomato-3" />
+        <div className="tomato-4" />
+        <div className="tomato-5" />
+        <div className="tomato-6" />
+        <div className="tomato-7" />
+        <div className="tomato-8" />
+        <div className="tomato-9" />
+        <div className="tomato-10" />
+      </div>
+    </>
+  );
+}
+
+export default Tomato;
---
+++ src/components/veggie/Veggie.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Veggie() {
+  return (
+    <>
+      <div className="veggies">
+        <div className="veg-1" />
+        <div className="veg-2" />
+        <div className="veg-3" />
+        <div className="veg-4" />
+        <div className="veg-5" />
+        <div className="veg-6" />
+        <div className="veg-7" />
+        <div className="veg-8" />
+        <div className="veg-9" />
+        <div className="veg-10" />
+        <div className="veg-11" />
+        <div className="veg-12" />
+        <div className="veg-13" />
+        <div className="veg-14" />
+      </div>
+    </>
+  );
+}
+
+export default Veggie;
